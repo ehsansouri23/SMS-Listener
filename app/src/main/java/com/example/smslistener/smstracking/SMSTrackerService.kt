66c9614a9b8cac6864bfa253dc18078506fc3020
Ehsan <ehsansouri23@gmail.com>
2020-08-16 16:31:43 +0430

    show foreground notification

--- app/src/main/java/com/example/smslistener/smstracking/SMSTrackerService.kt
+++ app/src/main/java/com/example/smslistener/smstracking/SMSTrackerService.kt
@@ -1,9 +1,17 @@
 package com.example.smslistener.smstracking
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Build
 import android.os.IBinder
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.smslistener.R
 
 class SMSTrackerService : Service() {
 
@@ -11,6 +19,7 @@ class SMSTrackerService : Service() {
 
     override fun onBind(intent: Intent?): IBinder? = null
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate() {
         super.onCreate()
         smsListener = SMSListener()
@@ -18,14 +27,31 @@ class SMSTrackerService : Service() {
             smsListener,
             IntentFilter("android.provider.Telephony.SMS_RECEIVED")
         )
+        startForeground(1, showNotification())
     }
 
     override fun onDestroy() {
         unregisterReceiver(smsListener)
+        super.onDestroy()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun showNotification(): Notification {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        NotificationChannel("SMS", "SMS", NotificationManager.IMPORTANCE_HIGH)
+            .let { notificationManager.createNotificationChannel(it) }
+        return NotificationCompat.Builder(applicationContext, "SMS").apply {
+            setContentTitle(getString(R.string.listening_to_SMSs))
+            setSmallIcon(R.drawable.ic_launcher_foreground)
+        }.build().also {
+            notificationManager.notify(1, it)
+        }
+
     }
 
     companion object {
-        val BODY = "body"
-        val FROM = "from"
+        const val BODY = "body"
+        const val FROM = "from"
     }
 }
