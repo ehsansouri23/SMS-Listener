34e3ee8d1d67a3c7a6eb15e55c47553e0ab4a8a2
Ehsan <ehsansouri23@gmail.com>
2020-08-16 17:03:09 +0430

    add url to send sms method

--- domain/src/main/java/com/example/domain/sms/SMSRepository.kt
+++ domain/src/main/java/com/example/domain/sms/SMSRepository.kt
@@ -2,5 +2,5 @@ package com.example.domain.sms
 
 interface SMSRepository {
 
-    suspend fun sendSMS(smsList: List<SMSModel>): Boolean
+    suspend fun sendSMS(url: String, smsList: List<SMSModel>): Boolean
 }
